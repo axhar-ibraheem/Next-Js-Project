ecc08d35d9f1ea8840ba361e02eb589001db533f
axhar-ibraheem <azharibraheem482@gmail.com>
2023-06-24 07:45:37 +0530

    navigating between pages

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
 const AboutUs = () => {
   return (
     <div>
       <h1>This is the about us page</h1>
+      <ul>
+        <li>
+          {" "}
+          <Link href="/aboutus/1"> Yash </Link>{" "}
+        </li>
+        <li>
+          {" "}
+          <Link href="/aboutus/2"> Vaibhav </Link>{" "}
+        </li>
+        <li>
+          {" "}
+          <Link href="/aboutus/3"> Suresh </Link>{" "}
+        </li>
+      </ul>
     </div>
   );
 };
